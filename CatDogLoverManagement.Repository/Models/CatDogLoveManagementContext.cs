6309963b2d611c8766d3e604c8fefe584eeee2f0
noir-reg <hungphamphu159@gmail.com>
2023-10-21 19:16:08 +0700

    Hide connection string

--- CatDogLoverManagement.Repository/Models/CatDogLoveManagementContext.cs
+++ CatDogLoverManagement.Repository/Models/CatDogLoveManagementContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace CatDogLoverManagement.Repository.Models
 {
@@ -31,10 +32,19 @@ namespace CatDogLoverManagement.Repository.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=noir-reg\\SQLEXPRESS01;Database=CatDogLoveManagement;User Id=sa;Password=12345;Encrypt=True;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
         }
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+            var strConn = config["ConnectionStrings:CatDogLoverManagementDb"];
 
+            return strConn;
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Animal>(entity =>
